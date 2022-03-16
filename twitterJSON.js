215290ed9ecb62fb3f7fc73435f8ce09ff7498cb
Tyson Martin <tyson.p.martin6701@tysonmartin.dev>
2022-03-15 19:57:17 -0500

    first commit

---
+++ twitterJSON.js
@@ -0,0 +1,49 @@
+
+
+let user1 = {
+    userName: '@elonmusk',
+    displayName: 'Elon Musk',
+    joinedDate: 'June 2009',
+    followingCount: 113,
+    followerCount: "76.5M",
+    avatarURL: `https://pbs.twimg.com/profile_images/1489375145684873217/3VYnFrzx_400x400.jpg`,
+    coverPhotoURL: 'assets/elonmusk-cover.jpeg',
+    tweets: [
+        {
+            text: 'I admit to judging books by their cover',
+            timestamp: '2/10/2022 00:01:20'
+        },
+        {
+            text: 'Starship to the moon',
+            timestamp: '2/09/2022 18:37:12'
+        },
+        {
+            text: 'Out on launch pad, engine swap underway',
+            timestamp: '2/09/2022 12:11:51'
+        }
+    ]
+};
+
+let user2 = {
+    userName: '@BillGates',
+    displayName: 'Bill Gates',
+    joinedDate: 'June 2009',
+    followingCount: 274,
+    followerCount: 53800000,
+    avatarURL: 'assets/billgates.jpg',
+    coverPhotoURL: 'assets/billgates-cover.jpeg',
+    tweets: [
+        {
+            text: 'Everybody asks, how is the next Windows coming along? But nobody asks how is Bill? :/',
+            timestamp: '2/10/2022 00:01:20'
+        },
+        {
+            text: 'Should I start tweeting memes? Let me know in a comment.',
+            timestamp: '2/09/2022 18:37:12'
+        },
+        {
+            text: 'In 2020, I read a book every hour.',
+            timestamp: '2/09/2022 12:11:51'
+        }
+    ]
+};
